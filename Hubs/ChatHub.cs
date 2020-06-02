68467fafb86dddd4e706d24684e292dd5af181e7
Steve Kaiser <stkaiser@microsoft.com>
2020-06-01 20:38:52 -0700

    initial commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+
+namespace chatserver.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private static readonly ConcurrentBag<string> _names = new ConcurrentBag<string>();
+
+        public async Task Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("broadcastMessage", Util.Sanitize(name), Util.MdToHtml(message));
+        }
+
+        public async Task RegisterName(string name)
+        {
+            // Add a name to the list.
+            string newName = Util.Sanitize(name);
+            if (!_names.Contains<string>(newName) && newName != string.Empty)
+            {
+                _names.Add(newName);
+            }
+            await SendNames();
+        }
+
+        public async Task SendNames()
+        {
+            // Send the name list to all clients.
+            await Clients.All.SendAsync("nameList", _names.ToArray<string>());
+        }
+
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Markdig;
+using Ganss.XSS;
+
+namespace chatserver
+{
+    public class Util
+    {
+        public static string MdToHtml(string input)
+        {
+            string output;
+            HtmlSanitizer hs = new HtmlSanitizer();
+            try
+            {
+                output = hs.Sanitize(Markdown.ToHtml(input));
+            }
+            catch (Exception) {
+                output = "Error rendering message";
+            }
+            return output;
+            
+        }
+
+        public static string Sanitize(string input)
+        {
+            string output;
+            HtmlSanitizer hs = new HtmlSanitizer();
+            try
+            {
+                output = hs.Sanitize(input);
+            }
+            catch (Exception)
+            {
+                output = "Sanitizer error";
+            }
+            return output;
+
+        }
+    }
+}
